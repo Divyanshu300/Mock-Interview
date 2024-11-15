4bae5190052284577f4ac217857c79c23a090123
Tanya Agarwal <135453589+tanya13agarwal@users.noreply.github.com>
2024-11-15 18:25:37 +0530

    Updated link in page.jsx

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,7 +1,7 @@
 import { UserButton } from "@clerk/nextjs";
 import React from "react";
 import AddNewInterview from "./_components/AddNewInterview";
-import InterviewList from "./_components/InterviewList";
+import InterviewList from "./_components/Interviewlist";
 
 const Dashboard = () => {
   return (
